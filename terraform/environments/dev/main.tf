9abd3911bc2de03f54074a458feb6ec8b423bff4
Sreya Vallamsetla <90157566+shre-code@users.noreply.github.com>
2026-09-24 12:48:40 -0500

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -170,18 +170,3 @@ resource "aws_cloudwatch_metric_alarm" "errors" {
   }
 }
 
-output "api_url" {
-  value = aws_apigatewayv2_stage.default.invoke_url
-}
-
-output "lambda_function" {
-  value = aws_lambda_function.api.function_name
-}
-
-output "dynamodb_table" {
-  value = aws_dynamodb_table.items.name
-}
-
-output "s3_bucket" {
-  value = aws_s3_bucket.artifacts.id
-}
